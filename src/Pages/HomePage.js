cc9d3141587d540bb67b19341454ff14e283edc5
JasonNelemans <jasonnelemans@hotmail.com>
2020-03-02 11:21:03 +0100

    added homepage buttons, linking to schedule and signup

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 export default function HomePage() {
   return (
     <div className="HomePage">
-      
+      <NavLink to="/schedule" activeStyle={{fontWeight: "bold", color: "black"}} exact={true}><button>Doctor Schedule</button></NavLink>
+      <NavLink to="/signup" activeStyle={{fontWeight: "bold", color: "black"}} exact={true}><button>Patient Signup</button></NavLink>
     </div>
   )
 }
