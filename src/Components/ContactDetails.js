497219a148e71c76a77a8fe4911745763c419c6c
JasonNelemans <jasonnelemans@hotmail.com>
2020-03-02 12:05:46 +0100

    added dynamic time to ContactDetails component

--- src/Components/ContactDetails.js
+++ src/Components/ContactDetails.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
 export default function ContactDetails() {
+
+  const today = new Date();
+  const time = today.getHours()
+  let hours;
+  if(time > 8 && time < 17) {
+    hours = 'open';
+  } else {
+    hours = 'closed';
+  }
+
   return (
     <div className="ContactDetails">
-      <p>We are <strong>open/closed</strong></p>
+      <p>We are <strong>{hours}</strong></p>
       <p>To make an appointment</p>
       <p>Call: 020 555 5555</p>
     </div>
