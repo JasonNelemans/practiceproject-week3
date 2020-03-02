f88483a08777336f85bf01c3c8938911672d0ec1
JasonNelemans <jasonnelemans@hotmail.com>
2020-03-02 15:34:27 +0100

    fetched data, passed down props to Patient, added click functionality to Patient component

--- src/Components/Patient.js
+++ src/Components/Patient.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-export default function Patient() {
+export default function Patient(props) {
+  const [ click, setClick ] = useState();
+  
+  const handleClick = () => {
+    const option = click ? setClick(false) : setClick(true)
+    return option
+  }
+
+  const display = click ? true : 'none'
+
   return (
     <div className="Patient">
-      
+      <p>Name: {props.firstName} {props.lastName} </p>
+      <p>id: {props.id} </p>
+      <p>date of birth: {props.dateOfBirth} </p>
+      <div style={{display: display}}>
+        <p>email: {props.email}</p>
+        <p>phone: {props.phoneNumber}</p>
+        <p>gender: {props.gender}</p>
+        <p> prescriptions: {props.prescriptions} </p>
+      </div>
+      <button onClick={handleClick}>show details </button>
     </div>
   )
 }
