5f238b6cb8af390cc3f4179f3e89f404d954ac52
JasonNelemans <jasonnelemans@hotmail.com>
2020-03-03 10:42:25 +0100

    added self-assessment

--- src/Pages/DoctorSchedule.js
+++ src/Pages/DoctorSchedule.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import ContactDetails from '../Components/ContactDetails'
 import DoctorAvailability from '../Components/DoctorAvailability';
 
